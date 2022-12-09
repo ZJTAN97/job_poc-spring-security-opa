d818d4cb88d1f37ddb542db7ae46944848041852
ZJ <zhijietan97@gmail.com>
2022-12-09 18:24:10 +0800

    add: starter spring boot 3

---
+++ src/main/java/com/poc/springsecurityopa/SpringsecurityopaApplication.java
@@ -0,0 +1,13 @@
+package com.poc.springsecurityopa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityopaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityopaApplication.class, args);
+	}
+
+}
